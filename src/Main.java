e9bbbbd0f47678c7cfddb539b06c196088a5c767
Teresa <primeytklsb@gmail.com>
2024-01-22 16:52:54 +0800

    Add function

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) throws InterruptedException {
         int choice;
-        System.out.println("Welcome to use TRSTool Java edition");
+        System.out.println("欢迎使用TRSTool Java edition(Beta)\\n");
         Thread.sleep(1000);
-        System.out.println("Please input which function you want to use:");
-        System.out.println("1.Transform long tool(beta)");
+        System.out.println("请输入你要使用的功能：\\n1.转换长度工具  2.转换鞋码工具 (new)");
         Scanner in = new Scanner(System.in);
         choice = in.nextInt();
         if (choice == 1)
         {
             Translong.translong();
+        }
+        else if (choice == 2){
+            Shoesize.transform();
         }
     }
 
