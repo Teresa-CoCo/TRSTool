dcab67464bebf4fa2e6de2b7984d4218213da429
Teresa-CoCo <primeytklsb@gmail.com>
2024-01-25 23:07:50 +0800

    Add GUIMinesweeper

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main{
         int choice;
         System.out.println("欢迎使用TRSTool Java edition(Beta)\\n");
         Thread.sleep(1000);
-        System.out.println("请输入你要使用的功能：\\n1.转换长度工具  2.转换鞋码工具 3.BMI健康指数计算工具 4.多功能电表 5.幸运数字与运势 6.二进制十进制互转 7.(chatgpt驱动)扫雷");
+        System.out.println("请输入你要使用的功能：\\n1.转换长度工具  2.转换鞋码工具 3.BMI健康指数计算工具 4.多功能电表 5.幸运数字与运势 6.二进制十进制互转 7.(chatgpt驱动)扫雷(命令行版本) 8.扫雷（GUI版本）");
         Scanner in = new Scanner(System.in);
         choice = in.nextInt();
         if (choice == 1)
@@ -30,6 +30,9 @@ public class Main{
         }
         else if (choice == 7){
             Minesweeper.start();
+        }
+        else if (choice == 8){
+            GUIMinesweeper.start();
         }
     }
 
