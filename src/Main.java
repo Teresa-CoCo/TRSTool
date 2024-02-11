4a7c614d670d27f9637c1fdb666388bfb5f29f83
Teresa-CoCo <primeytklsb@gmail.com>
2024-02-11 21:59:04 +0800

    Input and store to a string

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main{
         int choice;
         System.out.println("欢迎使用TRSTool Java edition(Beta)\\n");
         Thread.sleep(1000);
-        System.out.println("请输入你要使用的功能：\\n1.转换长度工具  2.转换鞋码工具 3.BMI健康指数计算工具 4.多功能电表 5.幸运数字与运势 6.二进制十进制互转 7.(chatgpt驱动)扫雷(命令行版本) 8.扫雷（GUI版本）9.打开记事本 10.成绩核算器 11.安带成绩核算器(未完成)");
+        System.out.println("请输入你要使用的功能：\\n1.转换长度工具  2.转换鞋码工具 3.BMI健康指数计算工具 4.多功能电表 5.幸运数字与运势 6.二进制十进制互转 7.(chatgpt驱动)扫雷(命令行版本) 8.扫雷（GUI版本）9.打开记事本 10.成绩核算器 11.安带成绩核算器(未完成)12.空格消除器（建设中）");
         Scanner in = new Scanner(System.in);
         choice = in.nextInt();
         if (choice == 1)
@@ -42,6 +42,9 @@ public class Main{
         }
         else if(choice == 11){
             ScoreCheck.start();
+        }
+        else if(choice == 12){
+            delSpace.start();
         }
     }
 
---
+++ src/delSpace.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class delSpace {
+    public static void start() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Please input the sentence\n");
+        String inpput = scanner.nextLine();
+        System.out.printf("Your input is %s", inpput);
+        
+    }
+}
