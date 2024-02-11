76f5762a2553b9dd406e354e0f1cc9658a7a76d1
Teresa-CoCo <primeytklsb@gmail.com>
2024-02-11 22:40:12 +0800

    Add Insert space function with two method

--- src/delSpace.java
+++ src/delSpace.java
@@ -5,7 +5,27 @@ public class delSpace {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Please input the sentence\n");
         String inpput = scanner.nextLine();
-        System.out.printf("Your input is %s", inpput);
-        
+        System.out.printf("Your input is %s\n\n", inpput);
+        char[] charArray = inpput.toCharArray();
+        //No.1
+        int i=0;
+        for(i=0;i<inpput.length();i++){
+            if (i%2==0&&i!=0) {
+                System.out.printf(" ");
+            }
+            // System.out.printf("%c",charArray[i]);
+        }
+        System.out.println("\n");
+        //No.2 Using buffer
+        StringBuilder two = new StringBuilder(1000);
+        // two.append(inpput);
+        for(int j = 0; j < inpput.length(); j++) {
+            two.append(inpput.charAt(j));
+            // 在每个字符后添加空格，除了最后一个字符
+            if (j%2==0&&j!=0) {
+                two.append(" ");
+            }
+        }
+        System.out.printf("%s",two);
     }
 }
