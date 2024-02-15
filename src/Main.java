8a99178ec9cefeb1aec8ecd6ac04ff010ad635bf
Teresa-CoCo <primeytklsb@gmail.com>
2024-02-15 22:43:04 +0800

    Add English support for Main

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) throws InterruptedException {
         int choice;
-        System.out.println("欢迎使用TRSTool Java edition\\n");
+        System.out.println("Welcome Using TRSTool Java edition\\n");
         Thread.sleep(1000);
+        //English version
+        // System.out.println("Please input which funtion you want to use:\n0.GUI edition(BUIDING)    1.Length transform  \n2.Shoe Size Transform    3.BMI Calculator \n4.Muti-funtion Electric Calculator    5.Lucky Number \n6.0101 to normal number    7.(drive by chatgpt)Minesweeper(Run in powershell) \n8.Minesweeper(GUI edition)    9.Open Notepad(windows) \n10.Score Calculator    11.AHU Score Calculator(UNFINISHED)\n12.Space delete tool    13.Array tool");
+        //Chinese version
         System.out.println("请输入你要使用的功能：\\n0.GUI版本（正在建设中）1.转换长度工具  2.转换鞋码工具 3.BMI健康指数计算工具 4.多功能电表 5.幸运数字与运势 6.二进制十进制互转 7.(chatgpt驱动)扫雷(命令行版本) 8.扫雷（GUI版本）9.打开记事本 10.成绩核算器 11.安带成绩核算器(未完成)12.空格消除器13.数组工具");
         Scanner in = new Scanner(System.in);
         choice = in.nextInt();
